6081b64f110d4acf17856aa8bd09fc29fc9a9ae5
Ilya <admin@redguy.ru>
2023-05-28 17:37:08 +0300

    Init

---
+++ index.js
@@ -0,0 +1,69 @@
+const {program} = require('commander');
+const express = require('express');
+const http = require("http");
+const https = require("https");
+
+function collect(value, previous) {
+    return previous.concat([value]);
+}
+
+program
+    .option('-v, --verbose', 'verbose output')
+    .option('-c, --config <file>', 'TODO: config file to use')
+    .option('-t, --timeout <ms>', 'TODO: timeout in ms [500]', '0')
+    .option('-H, --host <host>', 'host to connect to [localhost]', 'localhost')
+    .option('-p, --port <port>', 'port to listen [80]', '80')
+    .option('-m, --map <map>', 'a key:value enpoints mapping', collect, [])
+
+program.parse();
+
+const options = program.opts();
+if (options.verbose)
+    console.log(options);
+
+if (options.map.length === 0) {
+    options.map.push('/:/');
+}
+
+let app = express();
+
+options.map.forEach((mapping) => {
+    let [from, to] = mapping.split(':');
+    let host = options.host;
+
+    const router = express.Router();
+    router.all('*', (req, res) => {
+        let url = new URL(req.url, host);
+        url.pathname = url.pathname.replace(from, to);
+        if (options.verbose)
+            console.log(`Proxying ${(from + req.url).replaceAll("//", "/")} to ${url.href}`);
+        if(url.protocol === 'http:') {
+            http.get(url.href, (proxyRes) => {
+                res.writeHead(proxyRes.statusCode, proxyRes.headers);
+                proxyRes.pipe(res);
+            }).on('error', (err) => {
+                console.error(`Error while proxying request: ${err.message}`);
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        } else if(url.protocol === 'https:') {
+            https.get(url.href, (proxyRes) => {
+                res.writeHead(proxyRes.statusCode, proxyRes.headers);
+                proxyRes.pipe(res);
+            }).on('error', (err) => {
+                console.error(`Error while proxying request: ${err.message}`);
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        } else {
+            console.error(`Unsupported protocol: ${url.protocol}`);
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        }
+    });
+    app.use(from, router);
+});
+
+app.listen(options.port, () => {
+    console.log(`Listening on port ${options.port}`);
+});
