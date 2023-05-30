eec8324402019e0882ba42cf2aeb286a5e28f579
Ilya <admin@redguy.ru>
2023-05-30 21:28:00 +0300

    Config file support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {program} = require('commander');
 const express = require('express');
 const http = require("http");
 const https = require("https");
+const fs = require('fs');
 
 function collect(value, previous) {
     return previous.concat([value]);
@@ -23,19 +24,33 @@ const options = program.opts();
 if (options.verbose)
     console.log(options);
 
-if (options.map.length === 0) {
-    options.map.push('/:/');
-}
+if (options.config) {
+    if(!fs.existsSync(options.config)) {
+        fs.writeFileSync(options.config, JSON.stringify({routes: [{srcPath:"/",destPath:"/", destHost:"localhost"}]}, null, 4));
+    }
+    const config = JSON.parse(fs.readFileSync(options.config, 'utf8'));
+    if (config.routes) {
+        configuration.routes = config.routes;
+    }
+} else {
+    if (options.map.length === 0) {
+        options.map.push('/:/');
+    }
 
-for (let mapElement of options.map) {
-    configuration.routes.push({srcPath: mapElement.split(':')[0], destPath: mapElement.split(':')[1], destHost: options.host});
+    for (let mapElement of options.map) {
+        configuration.routes.push({
+            srcPath: mapElement.split(':')[0],
+            destPath: mapElement.split(':')[1],
+            destHost: options.host
+        });
+    }
 }
 
 configuration.routes = configuration.routes.sort((a, b) => {
     let aStr = a.srcPath;
     let bStr = b.srcPath;
-    if(!aStr.endsWith('/')) aStr += '/';
-    if(!bStr.endsWith('/')) bStr += '/';
+    if (!aStr.endsWith('/')) aStr += '/';
+    if (!bStr.endsWith('/')) bStr += '/';
     let aCount = aStr.split('/').length;
     let bCount = bStr.split('/').length;
     return bCount - aCount;
@@ -45,7 +60,7 @@ let app = express();
 const requestQueue = [];
 let isProcessing = false;
 
-if(options.timeout > 0) {
+if (options.timeout > 0) {
     app.use((req, res, next) => {
         requestQueue.push({req, res, next});
 
@@ -57,7 +72,7 @@ if(options.timeout > 0) {
 
 function processQueue() {
     if (requestQueue.length > 0) {
-        const { req, res, next } = requestQueue.shift();
+        const {req, res, next} = requestQueue.shift();
 
         next();
 
@@ -82,7 +97,7 @@ configuration.routes.forEach((route) => {
         url.pathname = url.pathname.replace(from, to);
         if (options.verbose)
             console.log(`Proxying ${(from + req.url).replaceAll("//", "/")} to ${url.href}`);
-        if(url.protocol === 'http:') {
+        if (url.protocol === 'http:') {
             http.get(url.href, (proxyRes) => {
                 res.writeHead(proxyRes.statusCode, proxyRes.headers);
                 proxyRes.pipe(res);
@@ -91,7 +106,7 @@ configuration.routes.forEach((route) => {
                 res.statusCode = 500;
                 res.end('Internal Server Error');
             });
-        } else if(url.protocol === 'https:') {
+        } else if (url.protocol === 'https:') {
             https.get(url.href, (proxyRes) => {
                 res.writeHead(proxyRes.statusCode, proxyRes.headers);
                 proxyRes.pipe(res);
